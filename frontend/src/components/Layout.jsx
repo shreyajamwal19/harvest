f380fb9009b66ae2d273ac6ade5b8e4810709196
Shreya Jamwal <shreya@harvest.dev>
2026-08-15 10:47:17 +0000

    Merge duplicate react-router-dom import in Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,6 +1,5 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import { AnimatePresence, motion } from 'framer-motion'
-import { useLocation } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
 
